c8349f7dc6b7734980096b817fd95f35972eb019
Joel <joelandapophis@gmail.com>
2021-09-25 10:18:43 -0500

    adding get mapping

--- Socket_Server/src/SocketServer.js
+++ Socket_Server/src/SocketServer.js
@@ -8,9 +8,18 @@ var status = {
     message: {}
 }
 
-const app = express().listen(port, () => {
+
+const app = express();
+app.get("/", (req, res) => {
+    let response = {
+        "status": "UP" 
+    };
+    res.send(response)
+});
+app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
+
 
 const socketServer = new Websocket.Server({ server: app });
 socketServer.on('connection', (socket) => {
